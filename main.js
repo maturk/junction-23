e3afc46967b025886cb9196060dd9185647ca599
jaaskelainenL <jaaskelainenL@protonmail.com>
2023-11-11 12:35:59 +0200

    Abstract points to point.js and update test server Point.js is a point which can be moved in the code. There's also a render loop now Also the test server didnt run with the parameters I defined earlier :( so I decided to change the command to start. I also removed the cache. Just in case we ever get to caching problems or whatnot.

--- main.js
+++ main.js
@@ -1,113 +1,107 @@
- 
-        const canvas = document.querySelector("canvas");
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
+import {Point} from "./point.js"
 
-        if (!navigator.gpu) {
-            throw new Error("WebGPU not supported on this browser.");
+const canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+if (!navigator.gpu) {
+    throw new Error("WebGPU not supported on this browser.");
+}
+
+const adapter = await navigator.gpu.requestAdapter();
+const device = await adapter.requestDevice();
+const context = canvas.getContext("webgpu");
+const canvasFormat = navigator.gpu.getPreferredCanvasFormat();
+context.configure({
+    device: device,
+    format: canvasFormat,
+});
+
+// GPUCommandEncoder provides an interface for recording GPU commands.
+const encoder = device.createCommandEncoder();
+
+// define how the vertex data is stored in memory so the gpu knows how to access it
+const vertexBufferLayout = {
+    arrayStride: 8,
+    attributes: [{
+        format: "float32x2",
+        offset: 0,
+        shaderLocation: 0, // Position, see vertex shader
+    }],
+};
+
+// our first webgpu shader
+const cellShaderModule = device.createShaderModule({
+    label: "Cell shader",
+    code: `
+        @vertex
+        fn vertexMain(@location(0) pos: vec2f) ->
+        @builtin(position) vec4f {
+        return vec4f(pos.x,pos.y, 0, 1);
         }
 
-        const adapter = await navigator.gpu.requestAdapter();
-        const device = await adapter.requestDevice();
-        const context = canvas.getContext("webgpu");
-        const canvasFormat = navigator.gpu.getPreferredCanvasFormat();
-        context.configure({
-            device: device,
-            format: canvasFormat,
-        });
+        @fragment
+        fn fragmentMain() -> @location(0) vec4f {
+        return vec4f(1, 0, 0, 1);
+        }
+    `
+});
 
-        // GPUCommandEncoder provides an interface for recording GPU commands.
-        const encoder = device.createCommandEncoder();
+// pipeline object
+const cellPipeline = device.createRenderPipeline({
+    label: "Cell pipeline",
+    layout: "auto",
+    vertex: {
+        module: cellShaderModule,
+        entryPoint: "vertexMain",
+        buffers: [vertexBufferLayout]
+    },
+    fragment: {
+        module: cellShaderModule,
+        entryPoint: "fragmentMain",
+        targets: [{
+            format: canvasFormat
+        }]
+    }
+});
 
-        // Our first shader and vertex code
-        // Maybe use Index Buffers for more trianges instead of hard coding. 
-        const vertices = new Float32Array([
-            //   X,    Y,
-            -0.8, -0.8, // Triangle 1 (Blue)
-            0.8, -0.8,
-            0.8, 0.8,
 
-            -0.8, -0.8, // Triangle 2 (Red)
-            0.8, 0.8,
-            -0.8, 0.8,
-        ]);
 
-        // allocate gpu memory for vertice data
-        const vertexBuffer = device.createBuffer({
-            label: "Cell vertices",
-            size: vertices.byteLength,
-            usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
-        });
+var point = new Point(-0.9, -0.9, 0.3);
 
-        // copy vertex data into gpu memory
-        device.queue.writeBuffer(vertexBuffer, /*bufferOffset=*/0, vertices);
+function draw(){
+    // Start a render pass 
+    const encoder = device.createCommandEncoder();
+    const pass = encoder.beginRenderPass({
+    colorAttachments: [{
+        view: context.getCurrentTexture().createView(),
+        loadOp: "clear",
+        clearValue: { r: 0, g: 0, b: 0.4, a: 1 }, // New line
+        storeOp: "store",
+        }],
+    });
 
-        // define how the vertex data is stored in memory so the gpu knows how to access it
-        const vertexBufferLayout = {
-            arrayStride: 8,
-            attributes: [{
-                format: "float32x2",
-                offset: 0,
-                shaderLocation: 0, // Position, see vertex shader
-            }],
-        };
+    // Draw the point
+    pass.setPipeline(cellPipeline);
+    point.move(0.1, 0.01);
+    
+    if(point.x > 1)
+        point.x = -1;
 
-        // our first webgpu shader
-        const cellShaderModule = device.createShaderModule({
-            label: "Cell shader",
-            code: `
-                @vertex
-                fn vertexMain(@location(0) pos: vec2f) ->
-                @builtin(position) vec4f {
-                return vec4f(pos.x,pos.y, 0, 1);
-                }
+    if(point.y > 1)
+        point.y = -1;
 
-                @fragment
-                fn fragmentMain() -> @location(0) vec4f {
-                return vec4f(1, 0, 0, 1);
-                }
-            `
-        });
+    var pointVertices = point.allocateBuffer(device);
+    pass.setVertexBuffer(0, pointVertices);
+    pass.draw(point.vertices.length/2);
 
-        // pipeline object
-        const cellPipeline = device.createRenderPipeline({
-            label: "Cell pipeline",
-            layout: "auto",
-            vertex: {
-                module: cellShaderModule,
-                entryPoint: "vertexMain",
-                buffers: [vertexBufferLayout]
-            },
-            fragment: {
-                module: cellShaderModule,
-                entryPoint: "fragmentMain",
-                targets: [{
-                    format: canvasFormat
-                }]
-            }
-        });
+    // End the render pass and submit the command buffer
+    pass.end();
+    device.queue.submit([encoder.finish()]);
 
-        // Actual rendering on gpu is done here with calls to the encoder object
-        const pass = encoder.beginRenderPass({
-            colorAttachments: [{
-                view: context.getCurrentTexture().createView(),
-                loadOp: "clear",
-                clearValue: { r: 0, g: 0, b: 0.4, a: 1 }, // New line
-                storeOp: "store",
-            }],
-        });
+}
 
-        pass.setPipeline(cellPipeline);
-        pass.setVertexBuffer(0, vertexBuffer);
-        pass.draw(vertices.length / 2); // 6 vertices
-
-        pass.end();
-        const commandBuffer = encoder.finish();
-
-        // the gpu queue performs all GPU commands
-        device.queue.submit([commandBuffer]);
-        device.queue.submit([encoder.finish()]);
-
+setInterval(draw, 1000/60);
 
 
 
